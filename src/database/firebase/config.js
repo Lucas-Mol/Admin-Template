85265eaecd3cfecb0267dc448ea3ba294ca161ff
Lucas Mol <lucasmolpro@outlook.com>
2023-07-20 01:22:00 -0300

    fix(): fixing env var

--- src/database/firebase/config.js
+++ src/database/firebase/config.js
@@ -4,13 +4,15 @@ import 'firebase/auth'
 if(!firebase.apps.length) {
 
     const firebaseConfig = {
-        apiKey: "",
-        authDomain: "",
-        projectId: "",
-        storageBucket: "",
-        messagingSenderId: "",
-        appId: ""
+        apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+        authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+        storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSANGING_SENDER_ID,
+        appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
       };
       
     firebase.initializeApp(firebaseConfig)
 }
+
+export default firebase
