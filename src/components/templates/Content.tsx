297923322c36752fe620237fe201f05d31e13a6f
Lucas Mol <lucasmolpro@outlook.com>
2023-07-21 16:41:54 -0300

    fix(): re styling content on pages

--- src/components/templates/Content.tsx
+++ src/components/templates/Content.tsx
@@ -7,7 +7,7 @@ interface ContentProps {
 export default function Content({children}: ContentProps) {
     return (
         <div className={`
-            flex flex-col mt-7
+            flex flex-col mt-7 mx-3 md:mx-5
         `}>
             {children}    
         </div>
